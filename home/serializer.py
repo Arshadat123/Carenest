f42315af1ec7079ef9a102a674c5f7be552ead63
Aswin Pradeep <aswinpradeepc@gmail.com>
2022-06-08 22:10:42 +0530

    new backends

--- home/serializer.py
+++ home/serializer.py
@@ -1,20 +1,39 @@
 from rest_framework import serializers
 from .models import *
+import datetime
 
 
 class GetHospitalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hospital
-        fields = ["id", "name"]
+        fields = ["id", "name", "location", "phone_number1", "phone_number2", "hospital_photo"]
 
 
 class GetDoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
-        fields = ["id", "name", "specialisation", "working_day", "time_schedule", "online_mode", "ima_no", "rating"]
+        fields = ["id", "name", "specialisation", "online_mode",  "rating", "doctor_photo"]
 
 
 class GetHomeNursesSerializer(serializers.ModelSerializer):
     class Meta:
         model = HomeNurses
-        fields = ["id", "name", "experience", "availability"]
+        fields = ["id", "name", "experience", "availability", "home_nurse_photo"]
+
+
+class GetWorkingTimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkingTime
+        fields = ["id", "day", "starting_time", "ending_time"]
+
+
+class GetMedicalHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MedicalHistory
+        fields = ["id", "date", "record", "prescription_photo"]
+
+
+class GetBloodDonationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BloodDonation
+        fields = ["id", "date", "blood_bank_name", "blood_bank_location", "phone_number1", "phone_number2"]
